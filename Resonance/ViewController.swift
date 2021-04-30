884aff3f3eead98acc96fe5a6173cff06c8b66cc
banjun <banjun@gmail.com>
2021-04-30 22:37:27 +0900

    Initial Commit

---
+++ Resonance/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Resonance
+//
+//  Created by BAN Jun on R 3/04/30.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
